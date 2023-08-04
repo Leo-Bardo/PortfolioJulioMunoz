76eb37e5c1d3a140e0ba3586e7dde305b2984057
Julio Muñoz <julioalb4488@gmail.com>
2023-08-04 16:43:45 -0600

    se resuben los archivos que no detecta el sistema git

--- enviar.php
+++ enviar.php
@@ -1,5 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD']=="POST") {
+
 		$telefono = $_POST['telefono'];
 		$correo = $_POST['correo'];
 		$mensaje = $_POST['mensaje'];
@@ -10,10 +11,6 @@ if ($_SERVER['REQUEST_METHOD']=="POST") {
 		$cuerpoMensaje = "Telefono: " . $telefono ."\n";
 		$cuerpoMensaje .= "Correo Electronico: " . $correo . "\n";
 		$cuerpoMensaje .= "Mensaje : \n" . $mensaje;
-
-		ini_set("SMTP", "smtp.gmail.com");
-    	ini_set("smtp_port", "587");
-    	ini_set("sendmail_from", "julioalb4488@gmail.com");
 
 		if (mail($destinatario, $asunto, $cuerpoMensaje)) {
 			echo "Mensaje enviado correctamente";
